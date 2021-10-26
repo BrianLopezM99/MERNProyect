beca17e0642146f004cfcd020d0f43ed9ffd4378
Brian Lopez <brianlopezmedina12@gmail.com>
2021-10-26 16:59:50 -0500

    Starting with context API

---
+++ src/components/tareas/ListadoTareas.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Tarea from "./Tarea";
+
+const ListadoTareas = () => {
+  const tareasProyecto = [
+    { nombre: "Elegir Plataforma", estado: true },
+    { nombre: "Elegir Colores", estado: false },
+    { nombre: "Elegir Plataforma de pago", estado: false },
+    { nombre: "Elegir Hosting", estado: true },
+  ];
+
+  return (
+    <>
+      <h2>Proyecto: Tienda virtual</h2>
+
+      <ul className="listado-tareas">
+        {tareasProyecto.length === 0 ? (
+          <li className="tarea">No hay tareas</li>
+        ) : (
+          tareasProyecto.map((tarea) => <Tarea tarea={tarea} />)
+        )}
+      </ul>
+      <button type="button" className="btn btn-eliminar">
+        Eliminar Proyecto &times;
+      </button>
+    </>
+  );
+};
+
+export default ListadoTareas;
